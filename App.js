4d9a6f5d8f6e7e16fb10027499f955af560dddc2
kurtreyn <kurtreyn@yahoo.com>
2022-02-15 22:27:22 -0500

    finish project

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
+import 'react-native-get-random-values';
+import {v4 as uuidv4} from 'uuid';
+import ListItem from './components/ListItem';
+import Header from './components/Header';
+import AddItem from './components/AddItem';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuidv4(), text: 'Milk'},
+    {id: uuidv4(), text: 'Eggs'},
+    {id: uuidv4(), text: 'Bread'},
+    {id: uuidv4(), text: 'Juice'},
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id != id);
+    });
+  };
+
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', {tedt: 'Ok'});
+    } else {
+      setItems(prevItems => {
+        return [{id: uuidv4(), text}, ...prevItems];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <Image
-        source={{uri: 'https://randomuser.me/api/portraits/men/1.jpg'}}
-        style={styles.img}
+      <Header title="Shopping List" />
+      <AddItem addItem={addItem} />
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
@@ -16,17 +47,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  },
-  img: {
-    width: 100,
-    height: 100,
-    borderRadius: 100 / 2,
+    paddingTop: 60,
   },
 });
 
